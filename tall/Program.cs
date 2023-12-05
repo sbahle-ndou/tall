542cf87294481df8a649af2dff24ea1c7d8484b1
SBICZA01\A253563 <sbahle.ndou@standardbank.co.za>
2023-12-05 12:28:04 +0200

    Add project files.

---
+++ tall/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+             
+                bool isMale = true;
+                if (isMale)
+                {
+                    Console.WriteLine("You are a male");
+                }
+                else
+                {
+                    Console.WriteLine("You are female");
+                }
+                Console.ReadLine();
+                bool isTall = false;
+
+                if (isTall && isMale)
+                {
+                    Console.WriteLine("You are both male and tall");
+
+                }
+                else if (!isTall && isMale)
+                {
+                    Console.WriteLine("You are not tall but you are male");
+                }
+                else
+                {
+                    Console.WriteLine("You are either not tall or not a male");
+                    Console.ReadLine();
+
+                }
+            }
+
+        }
+    }
+}
